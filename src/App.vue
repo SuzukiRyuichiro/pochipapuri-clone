09f67238fe9334fadafa9d307f9b4c40fdd4df27
Scooter <dragon.aka.scooter@gmail.com>
2022-07-13 01:34:38 +0900

    did stuff

--- src/App.vue
+++ src/App.vue
@@ -1,60 +1,64 @@
 <template>
   <v-app>
-    <v-app-bar
-      app
-      color="primary"
-      dark
-    >
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
-
-      <v-spacer></v-spacer>
-
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
-    </v-app-bar>
-
     <v-main>
-      <HelloWorld/>
+      <v-card min-height="100px" class="rounded-xl rounded-t-0 text-center mb-8" color="#FBF16A">
+        <v-card-title class="justify-center gap-4">
+          <span>AKT</span>
+          <img :src="require('./assets/pochipa.webp')" alt="Pochipa" id="pochipa">
+        </v-card-title>
+        <v-card-subtitle>AKTの公式LINEミニアプリ(仮)</v-card-subtitle>
+      </v-card>
+      <v-card class="mx-4 mb-10">
+        <v-card-title class="justify-center">秋田の天気</v-card-title>
+      </v-card>
+      <h3 class="text-center mb-5">お天気カメラ</h3>
+      <v-carousel height="144px" hide-delimiter-background hide-delimiters cycle>
+        <v-carousel-item v-for="item in items" :key="item.id">
+          <div v-html="item.content" class="d-flex justify-center"></div>
+        </v-carousel-item>
+      </v-carousel>
+      <v-card class="mx-4">
+        <v-card-title>秋田市の天気</v-card-title>
+        <v-card-subtitle>曇り</v-card-subtitle>
+        <v-card-text>28°</v-card-text>
+      </v-card>
     </v-main>
+    <bottom-nav />
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
+import BottomNav from './components/BottomNav'
 
 export default {
   name: 'App',
 
   components: {
-    HelloWorld,
+    BottomNav
   },
-
   data: () => ({
-    //
+    items: [{
+        id: "1",
+        content: '<iframe width="255" height="144" src="https://www.youtube.com/embed/DnFaI4l2bro?controls=0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>'
+      },
+      {
+        id: "2",
+        content: '<iframe width="255" height="144" src="https://www.youtube.com/embed/nX4JawVHTO4?controls=0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>'
+      }
+    ]
   }),
 };
 </script>
+
+<style lang="scss" scoped>
+  .v-main {
+    background-color: #F9F5BF;
+  }
+  #pochipa {
+    width: 30px;
+    height: 30px;
+  }
+  .gap-4 {
+    gap: 16px;
+  }
+</style>
---
+++ src/components/BottomNav.vue
@@ -0,0 +1,26 @@
+<template>
+  <v-bottom-navigation v-model="value" color="teal" grow>
+    <v-btn value="home">
+      <span>ホーム</span>
+
+      <v-icon>mdi-domain</v-icon>
+    </v-btn>
+    <v-btn value="recent">
+      <span>最新情報</span>
+
+      <v-icon>mdi-history</v-icon>
+    </v-btn>
+
+    <v-btn value="favorites">
+      <span>お気に入り</span>
+
+      <v-icon>mdi-heart</v-icon>
+    </v-btn>
+
+    <v-btn value="nearby">
+      <span>地域情報</span>
+
+      <v-icon>mdi-map-marker</v-icon>
+    </v-btn>
+  </v-bottom-navigation>
+</template>
